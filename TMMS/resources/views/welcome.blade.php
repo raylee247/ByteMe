28102a3441ba227fa58abae69f7f3d4f0d61821a
n2d8 <09orrp1@gmail.com>
2015-03-06 17:01:00 -0800

    added 3 buttons

--- TMMS/resources/views/welcome.blade.php
+++ TMMS/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>TMMS</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -40,7 +40,10 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">TMMS</div>
+				<button type="button">Apply As Mentor</button>
+				<button type="button">Apply As Student</button>
+				<button type="button">Admin Login</button>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
