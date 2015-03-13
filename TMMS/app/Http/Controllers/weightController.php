e4a6d1773159d988876938788279022fdfdb1b8e
Yiyang Kok <yiyangkok@gmail.com>
2015-03-13 13:41:49 -0700

    Revert "weighted parameters to array"
    This reverts commit 6bad621a48bf3c05761f7d58058540e66d8d67d4.

--- TMMS/app/Http/Controllers/weightController.php
+++ TMMS/app/Http/Controllers/weightController.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-session_start();
-
 class weightController extends Controller {
 
     public function __construct()
@@ -35,13 +33,8 @@ class weightController extends Controller {
 
     public function submitWeights()
     {
-        $weight = $_POST('weights');
-        $must = $_POST('musts');
-        $weights[] = explode(',' , $weight);
-        $musts[] = explode(',' , $must);
-
-        $_SESSION['weight'] = $weights;
-        $_SESSION['must'] = $musts;
+        $weights = $_POST('weights');
+        $weights[] = explode(',' , $weights);
 
     }
 
