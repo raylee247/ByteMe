d8d38b948709592cd73e10b4c4e3f86decd05e49
Miranda <miranda_tuet@hotmail.com>
2015-03-16 18:16:08 -0700

    updated table for uploadcsv, added new js for dialog

--- TMMS/resources/views/uploadcsv.blade.php
+++ TMMS/resources/views/uploadcsv.blade.php
@@ -5,18 +5,23 @@
 <div class="container">
  <form action="uploadCSV" method="post" enctype="multipart/form-data" >
   <input type="hidden" name="_token" value="{{ csrf_token() }}">
-  Select file to upload:
+  Select file to upload: 
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload CSV File" name="submit">
 </form>
 
+{{{ $test }}}
+
 @if (count($preview_header))
-<table id = "preview">
+<table id = "preview" class="table table-striped table-bordered" cellspacing="0" width="100%">
+  <thead>
   <tr>
     @foreach ($preview_header as $header)
     <th> {{$header}} </th>
     @endforeach
   </tr>
+</thead>
+<tbody>
   @foreach($preview_data as $participant)
   <tr>
     @foreach($participant as $data)
@@ -24,8 +29,8 @@
     @endforeach
   </tr>  
   @endforeach 
+</tbody>
 </table>
 @endif
-</div>
 
 @endsection
