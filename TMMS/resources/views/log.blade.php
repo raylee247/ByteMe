8220daa187ff15c8dddc962bd8c5f164f1338cf6
Yiyang Kok <yiyangkok@gmail.com>
2015-03-18 16:19:07 -0700

    little bit of formatting instead of using print_r to dump it

--- TMMS/resources/views/log.blade.php
+++ TMMS/resources/views/log.blade.php
@@ -20,8 +20,10 @@
 
 
     foreach($result as $single_result) {
-        print_r($single_result);
+        print("<strong>LogID:</strong> " . $single_result['logID'] . " <strong>Admin Name:</strong> " . $single_result['name'] . " <strong>Admin E-Mail:</strong> " . $single_result['email']);
         echo "<br>";
+        print("<strong>Action:</strong> " . $single_result['Action']);
+        echo "<br><br>";
     }
 
     ?>
