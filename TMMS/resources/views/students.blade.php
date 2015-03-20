a2a68d775b9b0c9ff0efc2c67332a1a790d76b97
aeedwko <edwin.mk.ko@gmail.com>
2015-03-20 16:30:54 -0700

    Basic search for students

--- TMMS/resources/views/students.blade.php
+++ TMMS/resources/views/students.blade.php
@@ -5,7 +5,7 @@
     <div class="row">    
         <div class="col-xs-8 col-xs-offset-2">
 
-<form action="studentSearch" method="post">
+<form action="students" method="post">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 		    <div class="input-group">
                 <div class="input-group-btn search-panel">
@@ -42,114 +42,30 @@
             </tr>
         </thead>
         <tbody>
-            <tr> 
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
-            </tr>
-            <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>San Francisco</td>
-                <td>66</td>
-                <td>2009/01/12</td>
-                <td>$86,000</td>
-            </tr>
-            <tr>
-                <td>Cedric Kelly</td>
-                <td>Senior Javascript Developer</td>
-                <td>Edinburgh</td>
-                <td>22</td>
-                <td>2012/03/29</td>
-                <td>$433,060</td>
-            </tr>
-            <tr>
-                <td>Airi Satou</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>33</td>
-                <td>2008/11/28</td>
-                <td>$162,700</td>
-            </tr>
-            <tr>
-                <td>Brielle Williamson</td>
-                <td>Integration Specialist</td>
-                <td>New York</td>
-                <td>61</td>
-                <td>2012/12/02</td>
-                <td>$372,000</td>
-            </tr>
-            <tr>
-                <td>Herrod Chandler</td>
-                <td>Sales Assistant</td>
-                <td>San Francisco</td>
-                <td>59</td>
-                <td>2012/08/06</td>
-                <td>$137,500</td>
-            </tr>
-            <tr>
-                <td>Rhona Davidson</td>
-                <td>Integration Specialist</td>
-                <td>Tokyo</td>
-                <td>55</td>
-                <td>2010/10/14</td>
-                <td>$327,900</td>
-            </tr>
-            <tr>
-                <td>Colleen Hurst</td>
-                <td>Javascript Developer</td>
-                <td>San Francisco</td>
-                <td>39</td>
-                <td>2009/09/15</td>
-                <td>$205,500</td>
-            </tr>
-            
-            <tr>
-                <td>Dai Rios</td>
-                <td>Personnel Lead</td>
-                <td>Edinburgh</td>
-                <td>35</td>
-                <td>2012/09/26</td>
-                <td>$217,500</td>
-            </tr>
-            <tr>
-                <td>Doris Wilder</td>
-                <td>Sales Assistant</td>
-                <td>Sidney</td>
-                <td>23</td>
-                <td>2010/09/20</td>
-                <td>$85,600</td>
-            </tr>
-            <tr>
-                <td>Angelica Ramos</td>
-                <td>Chief Executive Officer (CEO)</td>
-                <td>London</td>
-                <td>47</td>
-                <td>2009/10/09</td>
-                <td>$1,200,000</td>
-            </tr>
-            <tr>
-                <td>Donna Snider</td>
-                <td>Customer Support</td>
-                <td>New York</td>
-                <td>27</td>
-                <td>2011/01/25</td>
-                <td>$112,000</td>
-            </tr>
+            <?php
+                foreach($result as $single_result) {
+                    echo "<tr><td>"; 
+                    print_r($single_result['First name']);
+                    echo "</td>";
+                    echo "<td>"; 
+                    print_r($single_result['Family name']);
+                    echo "</td>";
+                    echo "<td>"; 
+                    print_r($single_result['studentNum']);
+                    echo "</td>";
+                    echo "<td>"; 
+                    print_r($single_result['csid']);
+                    echo "</td>";
+                    echo "<td>"; 
+                    print_r($single_result['yearStand']);
+                    echo "</td>";
+                    echo "<td>"; 
+                    print_r($single_result['email']);
+                    echo "</td></tr>";
+                }
+            ?>
         </tbody>
     </table>
-
 
 
 
