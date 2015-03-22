f6c7daeed5e3e1e396ff6fe0eb9fe0c6b050468f
Miranda <miranda_tuet@hotmail.com>
2015-03-21 20:10:09 -0700

    updating upload csv ui

--- TMMS/resources/views/uploadcsv.blade.php
+++ TMMS/resources/views/uploadcsv.blade.php
@@ -2,35 +2,79 @@
 
 @section('content')
 
+<div class="panel panel-info">
+  <div class="panel-heading"><b>Upload Participant Data</b></div>
+  <div class="panel-body">
+    <form action="uploadCSV" method="post" enctype="multipart/form-data" >
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      
+      <div class="col-lg-6 col-sm-6 col-12">
+        Select a file to upload: 
+        <div class="input-group">
+          <span class="input-group-btn">
+            <span class="btn btn-primary btn-file">
+              Browse <input type="file" name="fileToUpload" id="fileToUpload">
 
- <form action="uploadCSV" method="post" enctype="multipart/form-data" >
-  <input type="hidden" name="_token" value="{{ csrf_token() }}">
-  Select file to upload: 
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload CSV File" name="submit">
+            </span>
+          </span>
+          <input type="text" class="form-control" readonly>
+        </div>
+        <span class="help-block">
+          File format requires to be in .csv extension
+        </span>
+
+<!-- fix css -->
+      <span class="btn btn-primary btn-file">
+        <input type="submit" value="Upload CSV File" name="submit">
+      </span>
+
+      </div>
+
+
+
+
+
+<!--       <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload CSV File" name="submit"> -->
 </form>
 
 
 
-@if (count($preview_header))
-<table id = "preview" class="table table-striped table-bordered" cellspacing="0" width="100%">
-  <thead>
-  <tr>
-    @foreach ($preview_header as $header)
-    <th> {{$header}} </th>
-    @endforeach
-  </tr>
-</thead>
-<tbody>
-  @foreach($preview_data as $participant)
-  <tr>
-    @foreach($participant as $data)
-    <td> {{$data}} </td>
-    @endforeach
-  </tr>  
-  @endforeach 
-</tbody>
-</table>
-@endif
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+    @if (count($preview_header))
+    <table id = "preview" class="table table-striped table-bordered" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+          @foreach ($preview_header as $header)
+          <th> {{$header}} </th>
+          @endforeach
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($preview_data as $participant)
+        <tr>
+          @foreach($participant as $data)
+          <td> {{$data}} </td>
+          @endforeach
+        </tr>  
+        @endforeach 
+      </tbody>
+    </table>
+    @endif
+  </div>
+</div>
 
 @endsection
