8ac2c10a0b58963f94da63da62f583d5491406ad
Miranda <miranda_tuet@hotmail.com>
2015-03-21 21:38:56 -0700

    updated ui for download and uploadcsv

--- TMMS/resources/views/downloadcsv.blade.php
+++ TMMS/resources/views/downloadcsv.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
+<div class="panel panel-info">
+  <div class="panel-heading"><b>Download Participant Data</b></div>
+  <div class="panel-body">
+    <form action="uploadCSV" method="post" enctype="multipart/form-data" >
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+      I am downloading participant information from: //TODO: QUERY YEAR DATA FROM DB <br>
+      <div class="col-md-4">
+        <form>
+          <select class="form-control" name="year_csv" >
+                            <option id="2015">2015</option>
+                            <option id="2016">2016</option>
+                            <option id="2017">2017</option>
+                        </select>
+		</form><br>
+        <span class="input-group-btn">
+          <span class="btn btn-primary btn-file">
+            <span class="glyphicon glyphicon-download" aria-hidden="true"></span> 
+            Download File 
+
+
  <!-- Test download button -->
  {{--<form class="form-horizontal" role="form" method="POST" action="{{ url('downloadCSV') }}">--}}
  {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
@@ -14,6 +35,25 @@
   <input type="submit" value="Download CSV" name="Submit">
 </form>
 {{--                        <li></li> <a href={{url('download')}}>Test.pdf</a></li>--}}
+
+          </span>
+        </span>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @endsection
--- TMMS/resources/views/uploadcsv.blade.php
+++ TMMS/resources/views/uploadcsv.blade.php
@@ -7,14 +7,18 @@
   <div class="panel-body">
     <form action="uploadCSV" method="post" enctype="multipart/form-data" >
       <input type="hidden" name="_token" value="{{ csrf_token() }}">
-      
       <div class="col-lg-6 col-sm-6 col-12">
+        <form>
+          I am uploading participant information for: <br> //TODO: NEED TO POST VALUES AND SEND TO CONTROLLER
+          <label class="radio-inline"><input type="radio" name="category" <?php if (isset($category) && $category=="junior") echo "checked";?> value="junior">Juniors</label>
+          <label class="radio-inline"><input type="radio" name="category" <?php if (isset($category) && $category=="senior") echo "checked";?> value="senior">Seniors</label>
+          <label class="radio-inline"><input type="radio" name="category" <?php if (isset($category) && $category=="mentor") echo "checked";?> value="mentor">Mentors</label>
+        </form><br><br>
         Select a file to upload: 
         <div class="input-group">
           <span class="input-group-btn">
             <span class="btn btn-primary btn-file">
               Browse <input type="file" name="fileToUpload" id="fileToUpload">
-
             </span>
           </span>
           <input type="text" class="form-control" readonly>
@@ -22,59 +26,38 @@
         <span class="help-block">
           File format requires to be in .csv extension
         </span>
-
-<!-- fix css -->
-      <span class="btn btn-primary btn-file">
-        <input type="submit" value="Upload CSV File" name="submit">
-      </span>
-
+        <span class="input-group-btn">
+          <span class="btn btn-primary btn-file">
+            <span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Upload File <input type="submit" value="Upload CSV File" name="submit">
+          </span>
+        </span>
       </div>
-
-
-
-
-
-<!--       <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload CSV File" name="submit"> -->
-</form>
-
-
-
-
-</div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-    @if (count($preview_header))
-    <table id = "preview" class="table table-striped table-bordered" cellspacing="0" width="100%">
-      <thead>
-        <tr>
-          @foreach ($preview_header as $header)
-          <th> {{$header}} </th>
-          @endforeach
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($preview_data as $participant)
-        <tr>
-          @foreach($participant as $data)
-          <td> {{$data}} </td>
-          @endforeach
-        </tr>  
-        @endforeach 
-      </tbody>
-    </table>
-    @endif
+    </form>
   </div>
+</div>
+
+File Preview:
+<div class="container">   
+  @if (count($preview_header))
+  <table id = "preview" class="table table-striped table-bordered" cellspacing="0" width="auto">
+    <thead>
+      <tr>
+        @foreach ($preview_header as $header)
+        <th> {{$header}} </th>
+        @endforeach
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($preview_data as $participant)
+      <tr>
+        @foreach($participant as $data)
+        <td> {{$data}} </td>
+        @endforeach
+      </tr>  
+      @endforeach 
+    </tbody>
+  </table>
+  @endif
 </div>
 
 @endsection
