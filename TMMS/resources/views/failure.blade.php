0ebf80144825a064e521d9612df399f8ed403258
Miranda <miranda_tuet@hotmail.com>
2015-03-25 21:04:55 -0700

    added success and failure blade pages (not routed)

---
+++ TMMS/resources/views/failure.blade.php
@@ -0,0 +1,23 @@
+@extends('app')
+
+@section('content')
+
+<div class="panel panel-danger">
+	<div class="panel-heading">
+		<h2>Failure!</h2>
+		<div class="panel-body">
+			<!-- put message here -->
+		</div>
+	</div>
+</div>
+
+<style type="text/css">
+
+.panel-danger{
+	margin-right: 200px;
+	margin-left: 150px;
+	height:50%;
+}
+
+</style>
+@endsection
