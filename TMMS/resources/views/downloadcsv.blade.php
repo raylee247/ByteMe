eb02b27bfb670edd86a3d35c875e25321d3b275c
Miranda <miranda_tuet@hotmail.com>
2015-03-29 03:30:39 -0700

    added ui for download participant report

--- TMMS/resources/views/downloadcsv.blade.php
+++ TMMS/resources/views/downloadcsv.blade.php
@@ -2,113 +2,144 @@
 
 @section('content')
 
-    <div class="panel panel-info">
-        <div class="panel-heading"><b>Download Participant Data</b></div>
-        <div class="panel-body">
-            I am downloading participant information from:<br>
-            <div class="col-md-4">
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('downloadCSV') }}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    {{--<input type="file" name="Download Destination" id="fileToDownload">--}}
+<div class="panel panel-info">
+    <div class="panel-heading"><b>Download Participant Data</b></div>
+    <div class="panel-body">
+        I am downloading participant information from:<br>
+        <div class="col-md-4">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('downloadCSV') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                {{--<input type="file" name="Download Destination" id="fileToDownload">--}}
 
-                    <select class="form-control" name="year_csv" >
-                        <?php
-                        foreach($range as $range_index) {
-                            $option_year = $range_index['year'];
-                            echo "<option value=$option_year>$option_year</option>";
-                        }
-                        ?>
-                    </select><br>
-                   <span class="input-group-btn">
-          <span class="btn btn-primary btn-file">
-            <span class="glyphicon glyphicon-download" aria-hidden="true"></span>
-            Download File
-                  <input type="submit" value="Download CSV" name="Submit">
+                <select class="form-control" name="year_csv" >
+                    <?php
+                    foreach($range as $range_index) {
+                        $option_year = $range_index['year'];
+                        echo "<option value=$option_year>$option_year</option>";
+                    }
+                    ?>
+                </select><br>
+                <span class="input-group-btn">
+                  <span class="btn btn-primary btn-file">
+                    <span class="glyphicon glyphicon-download" aria-hidden="true"></span>
+                    Download File
+                    <input type="submit" value="Download CSV" name="Submit">
+                </span>
             </span>
-          </span>
-                    </form>
-                    {{--<li> <a href={{url('download')}}>Test.pdf</a></li>--}}
+        </form>
+        {{--<li> <a href={{url('download')}}>Test.pdf</a></li>--}}
 
-            </div>
-        </div>
     </div>
+</div>
+</div>
 
-    <div class="panel panel-info">
-        <div class="panel-heading"><b>Download Participant Emails</b></div>
-        <div class="panel-body">
-            I am downloading participant emails from:<br>
-            <div class="col-md-4">
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('downloadEmailAction') }}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    {{--<input type="file" name="Download Destination" id="fileToDownload">--}}
+<div class="panel panel-info">
+    <div class="panel-heading"><b>Download Participant Emails</b></div>
+    <div class="panel-body">
+        I am downloading participant emails from:<br>
+        <div class="col-md-4">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('downloadEmailAction') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                {{--<input type="file" name="Download Destination" id="fileToDownload">--}}
 
-                    <select class="form-control" name="year_csv" >
-                        <?php
-                        foreach($range as $range_index) {
-                            $option_year = $range_index['year'];
-                            echo "<option value=$option_year>$option_year</option>";
-                        }
-                        ?>
-                    </select><br>
-                   <span class="input-group-btn">
-          <span class="btn btn-primary btn-file">
-            <span class="glyphicon glyphicon-download" aria-hidden="true"></span>
-            Download Emails
-                  <input type="submit" value="Download Emails" name="Submit">
+                <select class="form-control" name="year_csv" >
+                    <?php
+                    foreach($range as $range_index) {
+                        $option_year = $range_index['year'];
+                        echo "<option value=$option_year>$option_year</option>";
+                    }
+                    ?>
+                </select><br>
+                <span class="input-group-btn">
+                  <span class="btn btn-primary btn-file">
+                    <span class="glyphicon glyphicon-download" aria-hidden="true"></span>
+                    Download Emails
+                    <input type="submit" value="Download Emails" name="Submit">
+                </span>
             </span>
-          </span>
-                </form>
-                {{--<li> <a href={{url('download')}}>Test.pdf</a></li>--}}
+        </form>
+        {{--<li> <a href={{url('download')}}>Test.pdf</a></li>--}}
 
-            </div>
-        </div>
     </div>
+</div>
+</div>
 
-    <div>
+<div class="panel panel-info">
+    <div class="panel-heading"><b>Download Participant Reports</b></div>
+    <div class="panel-body">
+        Download participant report from:<br>
+<!--         <div class="col-md-4">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('downloadEmailAction') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                {{--<input type="file" name="Download Destination" id="fileToDownload">--}}
+
+                <select class="form-control" name="year_csv" >
+                    <?php
+                    foreach($range as $range_index) {
+                        $option_year = $range_index['year'];
+                        echo "<option value=$option_year>$option_year</option>";
+                    }
+                    ?>
+                </select><br>
+                <span class="input-group-btn">
+                  <span class="btn btn-primary btn-file">
+                    <span class="glyphicon glyphicon-download" aria-hidden="true"></span>
+                    Download Report
+                    <input type="submit" value="Download Emails" name="Submit">
+                </span>
+            </span>
+        </form>
+        {{--<li> <a href={{url('download')}}>Test.pdf</a></li>--}}
+
+    </div> -->
+</div>
+</div>
+
+<div>
     <form class="form-horizontal" role="form" method="POST" action="{{ url('deleteYear') }}">
-    <div class="panel panel-danger">
+        <div class="panel panel-danger">
             <div class="panel-heading"><b>Delete Participant Data</b></div>
-                <div class="panel-body">
-                    I am deleting participant information from:<br>
-                    <div class="col-md-4">
-                        <select class="form-control" name="year_csv" >
-                            <?php
-                            foreach($range as $range_index) {
-                                $option_year = $range_index['year'];
-                                echo "<option value=$option_year>$option_year</option>";
-                            }
-                            ?>
-                        </select>
+            <div class="panel-body">
+                I am deleting participant information from:<br>
+                <div class="col-md-4">
+                    <select class="form-control" name="year_csv" >
+                        <?php
+                        foreach($range as $range_index) {
+                            $option_year = $range_index['year'];
+                            echo "<option value=$option_year>$option_year</option>";
+                        }
+                        ?>
+                    </select>
                     <br>
                     <span class="input-group-btn">
                         <span class="btn btn-danger btn-file" data-toggle="modal" data-target="#delete">
                             <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                                Delete
-                            </span>
+                            Delete
                         </span>
-                    </div>
-                </div>
-            </div>
-
-    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="deleteLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <form method="POST">
-            <div class="modal-content">
-                <div class="modal-body">Deleting the information will remove all associated data for that year within the system. Are you sure you want to continue?</div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    <span class="btn btn-primary btn-file">
-                        <span class="glyphicon glyphicon-confirm" aria-hidden="true"></span>
-                        Confirm
-                        <input type="submit" value="Confirm Delete" name="Submit">
                     </span>
-                    // TODO : delete data from db
                 </div>
             </div>
-            </form>
         </div>
-    </div>
+
+        <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="deleteLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <form method="POST">
+                    <div class="modal-content">
+                        <div class="modal-body">Deleting the information will remove all associated data for that year within the system. Are you sure you want to continue?</div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                            <span class="btn btn-primary btn-file">
+                                <span class="glyphicon glyphicon-confirm" aria-hidden="true"></span>
+                                Confirm
+                                <input type="submit" value="Confirm Delete" name="Submit">
+                            </span>
+                            // TODO : delete data from db
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </form>
-    </div>
+</div>
 
 @endsection
