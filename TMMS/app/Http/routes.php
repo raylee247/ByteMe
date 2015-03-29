ac9437104be7a2f3908c0275c26c9c698bf5f10c
Miranda <miranda_tuet@hotmail.com>
2015-03-28 23:58:12 -0700

    added routes and check for empty match result

--- TMMS/app/Http/routes.php
+++ TMMS/app/Http/routes.php
@@ -105,6 +105,7 @@ Route::get('weight', 'MakeMatching@loadParameters');
 Route::post('matchresult', 'weightController@matchresultindex');
 Route::get('savedmatches', 'weightController@savedmatchesindex');
 Route::post('savedmatches', 'weightController@savedmatchname');
+Route::get('kickoffmatches', 'weightController@kickoffindex');
 Route::post('kickoffmatches', 'weightController@savedmaxKickoff');
 Route::get('currentmatch', 'weightController@currentmatchindex');
 
