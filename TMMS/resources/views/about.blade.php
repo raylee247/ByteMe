157fbc927ee9bf3aedc1c00726f3e64a41c8d3e5
Yiyang Kok <yiyangkok@gmail.com>
2015-03-30 19:55:35 -0700

    we no gangsta anymore

--- TMMS/resources/views/about.blade.php
+++ TMMS/resources/views/about.blade.php
@@ -8,7 +8,11 @@
                 <div class="panel-heading">About</div>
 
                 <div class="panel-body">
-                    We be gangsta
+                    The TMMS is an independent and self-contained web-based application. The aim of this application is to match an industry mentor with a senior student and a junior student of the UBC CS Tri-Mentoring Program by using the information provided by the participants themselves. This application provides features that allow emails to be sent to a specific subset of participants and features that can generate or update a participant’s report.
+                    <br><br>
+                    Mentoring programs similar to the Tri-Mentoring program exist in many different universities. It is likely that such a system already exists; however, these systems are not available to the general public, so there is no reference for the TMMS.
+                    <br><br>
+                    Produced by ByteMe
                 </div>
             </div>
         </div>
