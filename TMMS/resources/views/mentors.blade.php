a7f5523c60976e90665d2897a9201a09e142e7b0
Miranda <miranda_tuet@hotmail.com>
2015-04-01 02:30:40 -0700

    fixed table alignments

--- TMMS/resources/views/mentors.blade.php
+++ TMMS/resources/views/mentors.blade.php
@@ -1,85 +1,84 @@
 @extends('app')
-
 @section('content')
-
 <style type="text/css">
-.panel-info {
+    .panel-info {
     margin-right: 0px;
+    }
+    table {
+    white-space:normal;
+    }
+    tr {
+    cursor: pointer;
 }
 </style>
-
 <div class="panel panel-default">
-  <div class="panel-body">
-    <div>    
+    <div class="panel-body">
         <div class="col-xs-8 col-xs-offset-2">
             <form action="mentors" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="input-group">
-                  <input type="hidden" name="search_param" value="all" id="search_param">         
-                  <input type="text" class="form-control" name="text" placeholder="Search with name, email, student number or CS ID">
-                  <span class="input-group-btn">
+                    <input type="hidden" name="search_param" value="all" id="search_param">         
+                    <input type="text" class="form-control" name="text" placeholder="Search with name or email">
+                    <span class="input-group-btn">
                     <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-            </div>
-        </form>
+                    </span>
+                </div>
+            </form>
+        </div>
+        <br><br><br>
+        <table id="example" class="table table-striped table-bordered table-hover" width="100%">
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Job</th>
+                    <th>Years of CS work</th>
+                    <th>Education Level</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $i = 0;
+                    foreach($result as $single_result)
+                    {
+                        $array[$i] = $result[$i]['pid'];
+                        $i++; 
+                        echo "<tr href='participant'><td>"; 
+                        print_r($single_result['First name']);
+                        echo "</td>";
+                        echo "<td>"; 
+                        print_r($single_result['Family name']);
+                        echo "</td>";
+                        echo "<td>"; 
+                        print_r($single_result['email']);
+                        echo "</td>";
+                        echo "<td>"; 
+                        print_r($single_result['job']);
+                        echo "</td>";
+                        echo "<td>"; 
+                        print_r($single_result['yearofcs']);
+                        echo "</td>";
+                        echo "<td>"; 
+                        print_r($single_result['edulvl']);
+                        echo "</td></tr>";
+                    }
+                    ?>
+            </tbody>
+        </table>
     </div>
 </div>
-<br>
-<table id="example" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
-    <thead>
-        <tr> 
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Job</th>
-            <th>Years of<br>CS work</th>
-            <th>Education Level</th>
-        </tr>
-    </thead>
-    <!-- PLACEHOLDER DATA FOR TABLE QUERY -->
-    <tbody>
-        <?php
-        $i = 0;
-        foreach($result as $single_result)
-        {
-            $array[$i] = $result[$i]['pid'];
-            $i++; 
-            echo "<tr href='participant'><td>"; 
-            print_r($single_result['First name']);
-            echo "</td>";
-            echo "<td>"; 
-            print_r($single_result['Family name']);
-            echo "</td>";
-            echo "<td>"; 
-            print_r($single_result['email']);
-            echo "</td>";
-            echo "<td>"; 
-            print_r($single_result['job']);
-            echo "</td>";
-            echo "<td>"; 
-            print_r($single_result['yearofcs']);
-            echo "</td>";
-            echo "<td>"; 
-            print_r($single_result['edulvl']);
-            echo "</td></tr>";
-        }
-        ?>
-    </tbody>
-</table>
-</div>
-</div>
-
 <script>
     $(document).ready(function(){
         $('table tr').click(function(){
             // index of row clicked 
             var row = ($(this).index());
-
+    
             // actual pid of the participant 
             var myvar = <?php
-                            echo json_encode($array);
-                        ?>;
-
+        echo json_encode($array);
+        ?>;
+    
             window.location.href = "participant" + "/" + myvar[row];
             return false;
         });
--- TMMS/resources/views/students.blade.php
+++ TMMS/resources/views/students.blade.php
@@ -1,96 +1,96 @@
 @extends('app')
-
 @section('content')
-
 <style type="text/css">
-.panel-info {
-    margin-right: 0px;
-}
+  .panel-info {
+  margin-right: 0px;
+  }
+  .table {
+  white-space:normal;
+  }
+  tr {
+  cursor: pointer;
+  }
 </style>
-
 <div class="panel panel-default">
   <div class="panel-body">
-          <div class="col-xs-8 col-xs-offset-2">
-            <form action="students" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="input-group">
-                    <div class="input-group-btn search-panel">
-                        <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                            <span id="search_concept">Filter by</span> <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="junior students">Junior students</a></li>
-                          <li><a href="senior students">Senior students</a></li>
-                          <li><a href="all">All students</a></li>
-                      </ul>
-                  </div>
-                  <input type="hidden" name="search_param" value="all" id="search_param">         
-                  <input type="text" class="form-control" name="text" placeholder="Search with name, email, student number or CS ID">
-                  <span class="input-group-btn">
-                    <button class="btn btn-default" onclick="myFunction()" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-            </div>
-        </form>
+    <div class="col-xs-8 col-xs-offset-2">
+      <form action="students" method="post">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="input-group">
+          <div class="input-group-btn search-panel">
+            <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+            <span id="search_concept">Filter by</span> <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="junior students">Junior students</a></li>
+              <li><a href="senior students">Senior students</a></li>
+              <li><a href="all">All students</a></li>
+            </ul>
+          </div>
+          <input type="hidden" name="search_param" value="all" id="search_param">         
+          <input type="text" class="form-control" name="text" placeholder="Search with name, email, student number or CS ID">
+          <span class="input-group-btn">
+          <button class="btn btn-default" onclick="myFunction()" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+          </span>
+        </div>
+      </form>
     </div>
-
-<br><br><br>
-<table id="example" class="table table-striped table-bordered table-hover" width="100%">
-    <thead>
-        <tr> 
-           <th>First Name</th>
-           <th>Last name</th>
-           <th>Student Number</th>
-           <th>CS ID</th>
-           <th>Year Standing</th>
-           <th>Email</th>
-       </tr>
-   </thead>
-   <tbody>
-    <?php
-    $i = 0; 
-    foreach($result as $single_result) {
-        $array[$i] = $result[$i]['pid'];
-        $i++; 
-
-        echo "<tr href='participant'><td>"; 
-        print_r($single_result['First name']);
-        echo "</td>";
-        echo "<td>"; 
-        print_r($single_result['Family name']);
-        echo "</td>";
-        echo "<td>"; 
-        print_r($single_result['studentNum']);
-        echo "</td>";
-        echo "<td>"; 
-        print_r($single_result['csid']);
-        echo "</td>";
-        echo "<td>"; 
-        print_r($single_result['yearStand']);
-        echo "</td>";
-        echo "<td>"; 
-        print_r($single_result['email']);
-        echo "</td></tr>";
-    }
-    ?>
-    </tbody>
-</table>
+    <br><br><br>
+    <table id="example" class="table table-striped table-bordered table-hover" width="100%">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last name</th>
+          <th>Student Number</th>
+          <th>CS ID</th>
+          <th>Year Standing</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          $i = 0; 
+          foreach($result as $single_result) {
+              $array[$i] = $result[$i]['pid'];
+              $i++; 
+          
+              echo "<tr href='participant'><td>"; 
+              print_r($single_result['First name']);
+              echo "</td>";
+              echo "<td>"; 
+              print_r($single_result['Family name']);
+              echo "</td>";
+              echo "<td>"; 
+              print_r($single_result['studentNum']);
+              echo "</td>";
+              echo "<td>"; 
+              print_r($single_result['csid']);
+              echo "</td>";
+              echo "<td>"; 
+              print_r($single_result['yearStand']);
+              echo "</td>";
+              echo "<td>"; 
+              print_r($single_result['email']);
+              echo "</td></tr>";
+          }
+          ?>
+      </tbody>
+    </table>
+  </div>
 </div>
-</div>
-
 <script>
-    $(document).ready(function(){
-        $('table tr').click(function(){
-            // index of row clicked 
-            var row = ($(this).index());
-
-            // actual pid of the participant 
-            var myvar = <?php
-                            echo json_encode($array);
-                        ?>;
-            window.location.href = "participant" + "/" + myvar[row];
-            return false;
-        });
-    });
+  $(document).ready(function(){
+      $('table tr').click(function(){
+          // index of row clicked 
+          var row = ($(this).index());
+  
+          // actual pid of the participant 
+          var myvar = <?php
+    echo json_encode($array);
+    ?>;
+          window.location.href = "participant" + "/" + myvar[row];
+          return false;
+      });
+  });
 </script>
-
 @endsection
