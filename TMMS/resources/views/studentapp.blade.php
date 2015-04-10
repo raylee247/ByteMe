437a517d1646b07d1eb33b85c383bad073561775
Miranda <miranda_tuet@hotmail.com>
2015-04-10 00:40:59 -0700

    fixed form checks

--- TMMS/resources/views/studentapp.blade.php
+++ TMMS/resources/views/studentapp.blade.php
@@ -10,6 +10,91 @@ div.error {
     box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
     padding: 2px 5px;
 }
+.studentapp{
+    position: absolute;
+
+}
+#input.required.error{
+        position: relative;
+    right: -755px;
+    top: -20px;
+    color:white;
+    z-index:1;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:20%;
+}
+#gender-error.error{
+    position: relative;
+    right: -755px;
+    top: -20px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:20%;
+}
+#cs_areasofinterest-error.error{
+    position: relative;
+    right: -850px;
+    top: -145px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:15%;
+}
+#coop-error.error{
+    position: relative;
+    right: -850px;
+    top: -30px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:15%;
+    }
+#participation-error.error{
+    position: relative;
+    right: -850px;
+    top: -30px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:15%;
+}
+#mentorgender-error.error{
+    position: relative;
+    right: -850px;
+    top: -30px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:15%;
+    }
+#yearofstudy-error.error{
+    position: relative;
+    right: -850px;
+    top: -30px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:15%;
+    }
+#programofstudy-error.error{
+    position: relative;
+    right: -850px;
+    top: -30px;
+    color:white;
+    background: #F00;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, .7);
+    padding: 2px 5px;
+    width:15%;
+}
 </style>
 <div class="row">
 <div class="col-md-10 col-md-offset-1">
@@ -30,55 +115,48 @@ div.error {
 
                     <label class="control-label col-sm-3">Email address:</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="email">
+                        <input type="text" class="form-control" name="email" placeholder="example@example.com">
                     </div>
                     <br><br>
                     <label class="control-label col-sm-3">Student number:</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="studentnum">
+                        <input type="text" class="form-control" name="studentnum" maxlength="8" placeholder="12345678">
                     </div>
                     <br><br>
                     <label class="control-label col-sm-3">Computer Science ID:</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="csid" minlength="4" maxlength="4" required>
+                        <input type="text" class="form-control" name="csid" minlength="4" maxlength="4" placeholder="ie. m9n9">
                     </div>
                     <br><br>
                     <label class="control-label col-sm-3">Given name:</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="givenname" minlength="2" required>
+                        <input type="text" class="form-control" name="givenname" placeholder="John">
                     </div>
                     <br><br>
                     <label class="control-label col-sm-3">Family name:</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="familyname" minlength="2" required>
+                        <input type="text" class="form-control" name="familyname" placeholder="Smith">
                     </div>
                     <br><br>
                     <label class="control-label col-sm-3">Phone:</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" minlength="10" maxlength="10" name="phone" required>
+                        <input class="form-control" minlength="10" maxlength="10" name="phone" placeholder="Format: 1234567890">
                     </div>
                     <br><br>
                     <label class="control-label col-sm-3">Phone (alternate):</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="phonealt" minlength="10" maxlength="10">
+                        <input class="form-control" name="phonealt" minlength="10" maxlength="10" placeholder="Format: 1234567890">
                     </div>
                     <br><br>
                     <div class="gender">
                         <label class="control-label col-sm-3">Gender:</label>
                         <label class="radio-inline"><input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male" required>Identify as male</label>
                         <label class="radio-inline"><input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female" required>Identify as female</label>
-                        <label class="radio-inline"><input type="radio" name="gender" class ="other" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other" required>Other (please specify)</label>
-                        <div class="genderother" id="otherfield">
-                            <label class="control-label col-sm-3">Gender (please specify):</label> 
-                            <div class="col-md-6 panel-collapse collapse in">
-                                <input type="text" class="form-control" name="genderother" value="otherfield">
-                            </div>
-                        </div>
                     </div>
                     <br>
                     <label class="control-label col-sm-3">Year of birth (Optional):</label>
                     <div class="col-md-6">
-                        <input type="text" class="form-control" name="birthyear">
+                        <input type="text" class="form-control" name="birthyear" placeholder="YYYY" minlength="4" maxlength="4">
                     </div>
                     <br><br><br>
                     <?php
@@ -105,18 +183,19 @@ div.error {
                             echo '<tr><td><center>' . $kickoff[$i] . '</center></td>';
                             //generate each row
                             for($j = 0; $j < $count; $j++){
-                                echo '<td><center><input type="radio" name="day' . ($i+1) . '"';
+                                echo '<td><center><input class="required" type="radio" name="day' . ($i+1) . '"';
                                 if(isset($day1) && $day1 == $kickoff[$i]){
                                     echo "checked";
                                 }
-                                $value = 'value="' . $kickoff[$i] . '" required></center></td>';
+                                $value = 'required value="' . $kickoff[$i] . '"></center></td>';
                                 echo $value;
                             }
                             echo '<td><center><input type="radio" name="day' . ($i+1) . '"';
                             if (isset($day1) && $day1== $kickoff[$i]) {
                                 echo "checked";
                             }
-                            echo $value = 'value="null"></center></td>';
+                            echo $value = 'value="null"></center>
+                            <label for="day' . ($i+1) . '" class="error" style="display:none;">Please choose one.</label></td>';
                             echo "</tr>";
                         }
                         
@@ -181,7 +260,7 @@ div.error {
 
                     <div class="form-inline">
                         <label class="control-label col-sm-3">Previously matched with a mentor and/or student mentee in the CS tri-mentoring program?</label><br>
-                        <select class="form-control" name="participation" required>
+                        <select class="form-control" name="participation">
                             <option value="">Select...</option>
                             <option id="previousmatched_no">No, I have not participated before</option>
                             <option id="previousmatched_junior">Yes, as a junior student</option>
@@ -189,7 +268,7 @@ div.error {
                             <option id="previousmatched_both">Both junior and senior student</option>
                         </select>
                     </div>
-                    <br><br>
+                    <br>
 
                     <div class="form-inline">
                         <label class="control-label col-sm-3">Co-op status:</label>
@@ -201,7 +280,7 @@ div.error {
                             <option value="coop_notacoopstudent">Not a co-op student</option>
                         </select>
                     </div>
-                    <br><br>
+                    <br>
                     {{--this is the extra questions part--}}
                     <?php
                         //questions comes in as an array holding arrays
@@ -214,7 +293,7 @@ div.error {
 
 
                         echo'<label class="control-label col-sm-3">' . $questions[$x][2] . '</label><br>
-                        <div class="col-md-6"><input type="checkbox" name="' . $questions[$x][1] . '[]" value="" checked="checked" style="display:none" required>';
+                        <div class="col-md-6"><input type="checkbox" name="' . $questions[$x][1] . '[]" value="" checked="checked" style="display:none">';
                                     $rawAnswer = $questions[$x][3]; //answers as a string comma seperated
                                     $answer = explode("," , $rawAnswer);
                                     $answerCount = count($answer);
@@ -229,7 +308,7 @@ div.error {
                                 case "text":
                                     echo '<div class="form-inline"><div class="col-sm-1"></div><label class="control-label pull-left">' . $questions[$x][2] . '</label>
                         <div class="col-md-4">
-                        <input type="text" class="form-control" name="' . $questions[$x][1] . '" required>
+                        <input type="text" class="form-control" name="' . $questions[$x][1] . '">
                         </div></div><br><br>';
                                     break;
                         
@@ -262,7 +341,7 @@ div.error {
                                                 echo "checked";
                                             }
                                             
-                                            $value ='value="' . $questions[$i][1] . '" required></center></td>';
+                                            $value ='value="' . $questions[$i][1] . '"></center></td>';
                         
                         
                                             echo $value;
@@ -279,7 +358,7 @@ div.error {
                         
                                 case "select": //this is dropdown
                                     echo '<div class="form-inline"><div class="col-sm-1"></div><label class="control-label pull-left">' . $questions[$x][2] . '<br></label>
-                        <div class="col-md-4"><select class="form-control" name="' . $questions[$x][1] . '" required>';
+                        <div class="col-md-4"><select class="form-control" name="' . $questions[$x][1] . '">';
                                     $rawAnswer = $questions[$x][3]; //answers as a string comma seperated
                                     $answer = explode("," , $rawAnswer);
                                     $answerCount = count($answer);
@@ -302,7 +381,7 @@ div.error {
                                     echo '<div class="col-sm-2"></div>
                         <label class="pull-left">' . $questions[$x][2] . '</label><br>
                         <center>
-                        <textarea rows="5" cols="130" name="' . $questions[$x][1] .  '" id="' . $questions[$x][1] .'" required></textarea>
+                        <textarea rows="5" cols="130" name="' . $questions[$x][1] .  '" id="' . $questions[$x][1] .'"></textarea>
                         </center>
                         <br>';
                                     break;
@@ -318,7 +397,7 @@ div.error {
                                         if(isset($questions[$x][1]) && $questions[$x][1]==$answer[$i]){
                                             echo "checked";
                                         }
-                                        echo ' value="' . $answer[$i] . '" required>' . $answer[$i] . '</label><br>';
+                                        echo ' value="' . $answer[$i] . '">' . $answer[$i] . '</label><br>';
                                     }
                         
                             }
@@ -328,7 +407,7 @@ div.error {
                         ?>                        
 
                     <div class="col-sm-1"></div>
-                        <label class="control-label pull-left">Computer Science areas of interest (please enter as comma-separated list):</label><br><br>
+                        <label class="pull-left">Computer Science areas of interest (please enter as comma-separated list):</label><br><br>
                         <div class="col-sm-1"></div>
                         <textarea rows="5" cols="130" name="cs_areasofinterest" id="cs_areasofinterest" required></textarea>
                         <br><br>
@@ -352,24 +431,63 @@ div.error {
             },
             studentnum: {
                 required: true,
+                number: true,
                 pattern: /^[0-9]{8}$/
-            },
-            phone: {
-                required: true,
-                digits: true
             },
             givenname: {
                 required: true,
-                minlength: 2
+                pattern: /(([a-zA-Z]+\-?)|([a-zA-Z]+\-?\ ))+/
             },
             familyname: {
-                required: true
+                required: true,
+                pattern: /(([a-zA-Z]+\-?)|([a-zA-Z]+\-?\ ))+/
+            },
+            phone: {
+                required: true,
+                digits: true,
+                minlength: 10,
+                pattern: /^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$/
+            },
+            phonealt: {
+                number: true,
+                pattern: /^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$/
             },
             csid: {
                 required: true,
                 pattern: /[a-z][0-9][a-z][0-9]/
             },
+            birthyear: {
+                number: true,
+                minlength: 4,
+                maxlength: 4,
+                pattern: /^[1-9]\d{3,}$/
+            },
+            gender: {
+                required: true
+            },
+            birthyear: {
+                number: true,
+                pattern: /^[1-2]\d{3,}$/
+            },
+            participation: {
+                required: true
+            },
+            <?php for($i = 0; $i < $count; $i++){
+                echo 'day' . ($i+1) .':{required:true},';
+            }
+                ?>
         },
+        errorPlacement: function(error, element) 
+    {
+        if ( element.is(":radio") ) 
+        {
+            error.appendTo( element.parents('.gender') );
+        }
+        else
+        { // This is the default behavior 
+            error.insertAfter( element );
+        }
+    },
         messages: {
             email: {
                 pattern: "Input is not a valid email address."
@@ -378,7 +496,10 @@ div.error {
                 pattern: "Student number must be 8 digits long."
             },
             csid: {
-                pattern: "CS ID should be like: x9y9"
+                pattern: "Input is not a valid CS ID."
+            },
+            birthyear: {
+                pattern: "Input is not a valid year."
             }
         }
     });
