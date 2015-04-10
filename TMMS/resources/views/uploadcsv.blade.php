0cf54a965dfd4f089b8bfa1f056e99c482f844da
Yiyang Kok <yiyangkok@gmail.com>
2015-04-09 18:22:26 -0700

    General fixes for upload/download functionality

--- TMMS/resources/views/uploadcsv.blade.php
+++ TMMS/resources/views/uploadcsv.blade.php
@@ -7,7 +7,7 @@
     
       <input type="hidden" name="_token" value="{{ csrf_token() }}">
       <div class="col-lg-6 col-sm-6 col-12">
-        <form action="uploadcsv_preview" method="post" enctype="multipart/form-data" >
+        <form action="uploadcsv" method="post" enctype="multipart/form-data" >
           Select a file to upload: 
           <div class="input-group">
             <span class="input-group-btn">
@@ -18,39 +18,25 @@
             <input type="text" class="form-control" readonly>
             <span class="input-group-btn">
               <span class="btn btn-info btn-file">
-                <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Preview <input type="submit" value="preview" name="submit"><!--specify value type here-->
+                <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Preview <input type="submit" value="preview" formaction="uploadcsv_preview" name="submit_preview"><!--specify value type here-->
               </span>
             </span>
           </div>
-            <?php if (!isset($category)) { echo '
-            <span class="help-block">
-        File format requires to be in .csv extension or CSV formatted .txt
-        </span>
-            I am uploading participant information for: <br>
-            <label class="radio-inline"><input type="radio" name="category" value="student">Students</label>
-            <label class="radio-inline"><input type="radio" name="category" value="mentor">Mentors</label>
-            <label class="radio-inline"><input type="radio" name="category" value="report">Reports</label>
-        ';} ?>
-        </form>
-
-      <?php if (isset($category)) { echo '
         <span class="help-block">
         File format requires to be in .csv extension or CSV formatted .txt
         </span>
-        <form action="uploadcsv_uploaded" method="post" enctype="multipart/form-data" >
           I am uploading participant information for: <br>
-            <label class="radio-inline"><input type="radio" name="category" '; if (isset($category) && $category=="student") echo "checked"; echo ' value="student">Students</label>
-            <label class="radio-inline"><input type="radio" name="category" '; if (isset($category) && $category=="mentor") echo "checked"; echo ' value="mentor">Mentors</label>
-            <label class="radio-inline"><input type="radio" name="category" '; if (isset($category) && $category=="report") echo "checked"; echo ' value="report">Reports</label>
+            <label class="radio-inline"><input type="radio" name="category" <?php if (isset($category) && $category=="student") echo "checked"; ?> value="student">Students</label>
+            <label class="radio-inline"><input type="radio" name="category" <?php if (isset($category) && $category=="mentor") echo "checked"; ?> value="mentor">Mentors</label>
+            <label class="radio-inline"><input type="radio" name="category" <?php if (isset($category) && $category=="report") echo "checked"; ?> value="report">Reports</label>
 
             <br><br>
           <span class="input-group-btn">
           <span class="btn btn-success btn-file">
-          <span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Upload File <input type="submit" formmethod="post" value="Upload CSV File" name="submit">
+          <span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Upload File <input type="submit" formmethod="post" formaction="uploadcsv_uploaded" value="Upload CSV File" name="submit_upload">
           </span>
           </span>
-        </form>';
-          } ?>
+        </form>
       </div>
     
   </div>
